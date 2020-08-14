9fa523b9957cbe5ec631754afce4091cd8d62e27
mskradde <skradde.marco@gmail.com>
2020-08-14 18:03:10 +0200

    Add searchbox

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import fetchWine from "./api/WineAPI";
 import List from "./components/List";
 import ListItem from "./components/ListItem";
+import LoadingScreen from "./components/LoadingScreen";
 
 function App() {
   const [allWines, setAllWines] = React.useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState("");
   async function getWines() {
     const newWines = await fetchWine();
     setAllWines(newWines);
-    console.log(allWines);
+    setIsLoading(false);
   }
 
   useEffect(() => {
     getWines();
   }, []);
+  if (isLoading || allWines === null) {
+    return <LoadingScreen />;
+  }
+  const filteredWines = allWines.filter((wine) => {
+    return wine.wine.toLowerCase().match(query.toLowerCase());
+  });
   return (
-    <div className="App">
+    <div className="app">
       <header className="header">
         Choose your favourite Wine{" "}
-        <input className="header__input" placeholder="Discover Wine" />
+        <input
+          value={query}
+          onChange={(event) => setQuery(event.target.value)}
+          className="header__input"
+          placeholder="Discover Wine"
+        />
       </header>
+
       <main className="wineList">
         <List>
-          {allWines?.map((wine) => {
+          {filteredWines?.map((wine) => {
             return (
               <ListItem key={wine.lwin} href={wine.href}>
                 {wine.wine}
