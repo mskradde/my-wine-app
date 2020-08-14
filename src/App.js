f9027c47d2beed57af3ecadd754ab30ffe4e366c
mskradde <skradde.marco@gmail.com>
2020-08-14 13:19:15 +0200

    Add wine api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import fetchWine from "./api/WineAPI";
 
 function App() {
+  const [allWines, setAllWines] = React.useState(null);
+  async function getWines() {
+    const newWines = await fetchWine();
+    setAllWines(newWines);
+    console.log(allWines);
+  }
+
+  useEffect(() => {
+    getWines();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <ul>
+          {allWines?.map((wine) => {
+            return console.log(wine.wine);
+          })}
+        </ul>
+      </main>
+      <header>My Wine App</header>
+      <footer>Footer</footer>
     </div>
   );
 }
