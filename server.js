7e1acefe12267f9b8491d13337c73b1e957e078c
mskradde <skradde.marco@gmail.com>
2020-08-28 16:28:01 +0200

    Send get request to server

--- server.js
+++ server.js
@@ -3,11 +3,25 @@ const { MongoClient } = require("mongodb");
 
 const client = new MongoClient(process.env.Mongo_URI);
 
-async function run() => {
-    try {
-        await client.connect();
-        const database = client.db(process.env.Mongo_DB_NAME);
-    } finally {
-        await client.close();
-    }
-}
+const express = require("express");
+const { response } = require("express");
+const app = express();
+
+const port = 3333;
+app.get("/", (request, response) => {
+  console.log("Request /");
+  response.send("Request sleep");
+});
+
+app.listen(port, () => {
+  console.log(`dubidu I´m listen to you on http://localhost:${port}`);
+});
+
+// async function run() => {
+//     try {
+//         await client.connect();
+//         const database = client.db(process.env.Mongo_DB_NAME);
+//     } finally {
+//         await client.close();
+//     }
+// }
