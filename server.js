b79b128e3cc8f99f609c788a6b32b5f661ecfd87
mskradde <skradde.marco@gmail.com>
2020-08-28 13:30:41 +0200

    Import mongodb to server.js

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
 require("dotenv").config();
-const { mongoClient } = require("mongodb");
+const { MongoClient } = require("mongodb");
+
+const client = new MongoClient(process.env.Mongo_URI);
+
+async function run() => {
+    try {
+        await client.connect();
+        const database = client.db(process.env.Mongo_DB_NAME);
+    } finally {
+        await client.close();
+    }
+}
